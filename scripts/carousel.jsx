5f7b705fdec816e91f73528a2eb41345409f95f7
April Arcus <april.arcus@gmail.com>
2015-03-06 16:54:03 -0800

    add eslintrc

---
+++ scripts/carousel.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+const React = require('react');
+
+const Carousel = React.createClass({
+  render() {
+    return <h1>Hello, world.</h1>;
+  }
+});
+
+module.exports = Carousel;
---
+++ scripts/index.jsx
@@ -0,0 +1,6 @@
+'use strict';
+
+const React = require('react'),
+      Carousel = require('./carousel');
+
+React.render(<Carousel />, document.body);
