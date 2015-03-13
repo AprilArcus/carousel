0fd8bd9ff5871b670b4d3dd4794fdc3dd7dc5a18
April Arcus <april.arcus@gmail.com>
2015-03-13 11:04:40 -0700

    translating bootstrap.less's logic more faithfully

--- scripts/components/Button.jsx
+++ scripts/components/Button.jsx
@@ -39,12 +39,11 @@ export default React.createClass({
 
     if (this.props.disabled) {
       style.color = variables.btn.link.disabled.color;                  // ln 117
-      return style; // early return
-    }
-
-    if (this.state.hover || this.state.focus) {
-      style.color = variables.link.hover.color;                         // ln 109
-      style.textDecoration = variables.link.hover.decoration;           // ln 110
+    } else {
+      if (this.state.hover || this.state.focus) {
+        style.color = variables.link.hover.color;                       // ln 109
+        style.textDecoration = variables.link.hover.decoration;         // ln 110
+      }
     }
 
     return style;
@@ -55,32 +54,26 @@ export default React.createClass({
 
     // original less styles implemented in
     // https://github.com/twbs/bootstrap/blob/9ed9eb97ee09e514da942d680dac032628124816/less/mixins/buttons.less
-    // ll 6-44
+    // ll 6-54
     // https://github.com/twbs/bootstrap/blob/65721f531536e05026b1ae5f0359955c78d13156/less/buttons.less
     // ll 57-81
     const style = {
       color: variables.btn[this.props.bsStyle].color,                   // ln 7
-      backgroundColor: variables.btn[this.props.bsStyle].bg,            //  8
+      backgroundColor: variables.btn[this.props.bsStyle].bg,            // ln 8
       borderColor: variables.btn[this.props.bsStyle].border,            // ln 9
       borderRadius: variables.border_radius[this.props.bsSize],
-      fontWeight: variables.btn.font_weight,                            // ln 12
+      fontWeight: variables.btn.font_weight                             // ln 12
     };
 
-    if (this.props.disabled) {
-      style.cursor = variables.cursor.disabled;                         // ln 49
-      style.pointerEvents = 'none';
-      style.opacity = 0.65;                                             // ln 51
-      return style; // early return
-    }
-
-    if (this.state.hover || this.state.active || this.state.focus) {    // ll 11-15
-      style.backgroundColor = darken(style.backgroundColor, 10);        // ln 18
-      style.borderColor = darken(style.borderColor, 12);                // ln 19
-    }
-
-    if (this.state.active) {
-      style.outline = 0;                                                // ln 41
-      style.boxShadow = 'inset 0 3px 5px rgba(0,0,0,.125)';             // ln 43
+    if (!this.props.disabled) {
+      if (this.state.hover || this.state.active || this.state.focus) {  // ll 11-15
+        style.backgroundColor = darken(style.backgroundColor, 10);      // ln 18
+        style.borderColor = darken(style.borderColor, 12);              // ln 19
+      }
+      if (this.state.active) {
+        style.outline = 0;                                              // ln 41
+        style.boxShadow = 'inset 0 3px 5px rgba(0,0,0,.125)';           // ln 43
+      }
     }
 
     return style;
@@ -104,6 +97,12 @@ export default React.createClass({
       borderColor: 'transparent',                                       // ln 18
       whiteSpace: 'nowrap'                                              // ln 19
     };
+
+    if (this.props.disabled) {
+      baseStyle.cursor = variables.cursor.disabled;                     // ln 49
+      baseStyle.pointerEvents = 'none';                                 // ln 50
+      baseStyle.opacity = 0.65;                                         // ln 51
+    }
 
     if (this.props.bsStyle === 'link') {
       return Object.assign(baseStyle, this.linkStyle(), this.props.style);
