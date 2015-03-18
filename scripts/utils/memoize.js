611c580184b40f3d2d87001c53e156f4331dfad9
April Arcus <april.arcus@gmail.com>
2015-03-18 07:46:02 -0700

    now we can memoize null arg functions which return undefined

--- scripts/utils/memoize.js
+++ scripts/utils/memoize.js
@@ -1,51 +1,54 @@
 /* eslint-env es6 */
 function iterativeLookup(args, cache, callback) {
   for (let i = 0; i < args.length; ++i) {
+    const arg = args[i]
     if (i < args.length - 1) {
-      if (!cache.has(args[i])) {
-        cache.set(args[i], new Map());
+      if (!cache.has(arg)) {
+        cache.set(arg, new Map());
       }
-      cache = cache.get(args[i]);
+      cache = cache.get(arg);
     } else {
-      if (!cache.has(args[i])) {
-        cache.set(args[i], callback());
+      if (!cache.has(arg)) {
+        cache.set(arg, callback());
       }
-      return cache.get(args[i]);
+      return cache.get(arg);
     }
   }
 }
 
 function recursiveLookup(args, cache, callback) {
+  const arg = args[0]
   // base case
   if (args.length === 1) {
-    if (!cache.has(args[0])) {
-      cache.set(args[0], callback());
+    if (!cache.has(arg)) {
+      cache.set(arg, callback());
     }
-    return cache.get(args[0]);
+    return cache.get(arg);
   }
   // recursive case
-  if (!cache.has(args[0])) {
-    cache.set(args[0], new Map());
+  if (!cache.has(arg)) {
+    cache.set(arg, new Map());
   }
   // Testing out Babel's tail call optimization. This should be
   // transpiled into a while() loop. Repetitive sub-slicing of args
   // will cause heap fragmentation; the iterative version will be
   // be more performant in practice.
-  recursiveLookup(args[1,-1], cache.get(args[0]), callback);
+  recursiveLookup(args[1,-1], cache.get(arg), callback);
 }
 
 export default function memoize(fn, context) {
   const multiArgumentCache = new Map();
-  let nullArgumentCache;
+  let nullArgumentCache = {empty: true, value: undefined};
 
   return function(...args) {
     const callback = fn.bind(context, ...args);
 
     if (args.length === 0) {
-      if (nullArgumentCache === undefined) {
-        nullArgumentCache = callback.call(context);
+      if (nullArgumentCache.empty === true) {
+        nullArgumentCache.value = callback.call(context);
+        nullArgumentCache.empty = false;
       }
-      return nullArgumentCache
+      return nullArgumentCache.value;
     } else {
       return iterativeLookup(args, multiArgumentCache, callback);
     }
